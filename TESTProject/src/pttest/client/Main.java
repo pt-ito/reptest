7817047b2a2336f515f35eeafac8dce224e2373b
ayumi.ito <ayumi.ito@prime-tech.co.jp>
2014-06-22 20:48:36 +0900

    second commit

--- TESTProject/src/pttest/client/Main.java
+++ TESTProject/src/pttest/client/Main.java
@@ -5,7 +5,7 @@ import com.google.gwt.core.client.EntryPoint;
 public class Main implements EntryPoint {
 
     public void onModuleLoad() {
-        // TODO Auto-generated method stub
+        //  ”½‰fƒeƒXƒg
 
     }
 }
